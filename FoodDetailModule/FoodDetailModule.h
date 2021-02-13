b23e68a623bab435d319ba8bb35a1137b90616fa
aldo.vernando <aldo.vernando@gmail.com>
2021-02-13 19:19:46 +0700

    Initial Commit

---
+++ FoodDetailModule/FoodDetailModule.h
@@ -0,0 +1,18 @@
+//
+//  FoodDetailModule.h
+//  FoodDetailModule
+//
+//  Created by aldo vernando on 13/02/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FoodDetailModule.
+FOUNDATION_EXPORT double FoodDetailModuleVersionNumber;
+
+//! Project version string for FoodDetailModule.
+FOUNDATION_EXPORT const unsigned char FoodDetailModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FoodDetailModule/PublicHeader.h>
+
+
